0f4e83f61ae71dc4c8dcb16b3f82c8fb08e7ebb4
Nicole <157562160+legaspinicole@users.noreply.github.com>
2026-09-23 00:34:50 +0800

    Connect expense and category functions

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
+from expense import add_expense, get_expenses, calculate_total
+from category import get_categories
+
+
 def main():
     print("Expense Tracker")
-    print("System started.")
+    print("----------------")
+
+    add_expense(
+        180,
+        "Food",
+        "Lunch",
+        "09/23/2026"
+    )
+
+    print("\nRecent Transactions:")
+
+    for expense in get_expenses():
+        print(
+            expense["description"],
+            "- ₱",
+            expense["amount"]
+        )
+
+    print("\nTotal spent: ₱", calculate_total())
+
+    print("\nCategories:")
+    print(get_categories())
 
 
 if __name__ == "__main__":
