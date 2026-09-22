4d1e65c89d9eecf8d68d7ffe20d412ed56e4c771
Nicole <157562160+legaspinicole@users.noreply.github.com>
2026-09-23 00:34:10 +0800

    Add category functions

---
+++ category.py
@@ -0,0 +1,14 @@
+categories = [
+    "Food",
+    "Transportation",
+    "Shopping",
+    "Bills"
+]
+
+
+def get_categories():
+    return categories
+
+
+def add_category(category):
+    categories.append(category)
